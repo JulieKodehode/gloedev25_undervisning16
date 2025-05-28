624c44123915d2a2f2fde43b9ce4068a67e5bb7e
JulieKodehode <julie.kodehode@gmail.com>
2025-05-28 12:04:57 +0200

    After lesson

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("Hello?");
+
+const submitButton = document.getElementById("submitButton");
+console.log(submitButton);
+
+submitButton.addEventListener("click", function (event) {
+    console.log("Button is clicked?");
+    event.preventDefault();
+
+    const inputNumber = document.getElementById("inputNumber").value;
+    console.log(inputNumber);
+
+    const selectOptions = document.getElementById("selectOptions").value;
+    console.log(selectOptions);
+
+    let amount;
+
+    let displayResults = document.getElementById("displayResults");
+
+    if (selectOptions === "pickUp") {
+        // Do this
+        amount = inputNumber * 10;
+        console.log("Hent " + amount + " epler.");
+        
+        displayResults.textContent = "Hent " + amount + " epler.";
+    } else if (selectOptions === "deliver") {
+        // Do that
+        amount = inputNumber * 11;
+        console.log("Det leveres " + amount + " epler.");
+
+        displayResults.textContent = "Det leveres " + amount + " epler.";
+    } else {
+        // Do something else
+        console.error("Noe er feil");
+    }
+});
